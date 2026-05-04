4e4476e82c3a757f11d26ba5ec45e773ff502b8a
Prahlad Bhakat <prahladbhakat05@gmail.com>
2026-05-04 11:23:13 +0530

    configure production cors for deployment

--- backend/src/main/java/com/prahlad/ecommerce/config/SecurityConfig.java
+++ backend/src/main/java/com/prahlad/ecommerce/config/SecurityConfig.java
@@ -63,8 +63,8 @@ public class SecurityConfig
     {
 
         CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of("*"));
+//        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
